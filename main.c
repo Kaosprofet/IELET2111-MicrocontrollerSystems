48d8a7771ff7f9e2ab70c1f8c5bd965306f5fb79
Kaosprofet <69542307+Kaosprofet@users.noreply.github.com>
2021-04-23 21:41:15 +0200

    Add files via upload
    Started project

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * AVRSensor3000.c
+ *
+ * Created: 23/04/2021 21:37:14
+ * Author : henri
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
