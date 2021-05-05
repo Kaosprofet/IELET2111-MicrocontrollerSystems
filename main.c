6eb962077e360498a3ad1eb834fd0157c7176968
Henrik M. Arnesen <henrimar@stud.ntnu.no>
2021-05-05 16:41:22 +0200

    Servo done

--- main.c
+++ main.c
@@ -16,21 +16,29 @@
 #define SERVO_PERIOD F_CPU/1000000*20000/T1_Prescale	// Calculates servo period in number of clock cycles
 #define SERVO_MAX F_CPU/1000000*2500/T1_Prescale		// Servo max value
 #define SERVO_MIN F_CPU/1000000*600/T1_Prescale			// Servo min value
+#define ADC_MAX 1023									// ADC max value
+#define ADC_MIN 0										// ADC min value
+#define SERVO_PIN DDB1									// Servo pin B1 (pin 9)
 
+uint16_t temp = 0;
 
 void timer1PWMInit(void);
 void calServo(void);
-void runServo(void);
+void runServo(uint16_t temp);
 
 int main(void) {
     timer1PWMInit();
+	temp = 1023;
     while (1) {
-		calServo(); // Calibrate the servo to find min/max values
+		//calServo(); // Calibrate the servo to find min/max values
+		
+		//temp = // ADC value
+		runServo(temp);
     }
 }
 
 void timer1PWMInit(void) {
-	DDRB = (1<<DDB1);											// Setting PB1 as output
+	DDRB = (1<<SERVO_PIN);										// Setting PB1 as output
 	TCCR1A = (1<<COM1A1)|(0<<COM1A0)|(1<<WGM11)|(0<<WGM10);		// Setting fast PWM and non-inverting mode
 	TCCR1B = (1<<CS11)|(1<<WGM13)|(1<<WGM12);					// Fast PWM and set prescaling
 	ICR1 = SERVO_PERIOD;
@@ -43,6 +51,8 @@ void calServo(void) {
 	_delay_ms(1000);
 }
 
-void runServo(void) {
-	
+void runServo(uint16_t temp) {
+	temp = 1023-temp;																// Compensating for physical dial
+	temp = (temp - 0) * (SERVO_MAX - SERVO_MIN) / (ADC_MAX - ADC_MIN) + SERVO_MIN;	// Mapping value for servo
+	OCR1A = temp;																	// Writing to servo
 }
