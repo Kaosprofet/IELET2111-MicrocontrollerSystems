8cf4fed3589b3b9b3e79546699a2a5b3e56d9f42
Simen Dager Sneve <smndagersneve@gmail.com>
2021-05-07 20:35:34 +0200

    Added the second ADC

--- adc.c
+++ adc.c
@@ -3,7 +3,7 @@
 #include "adc.h"
 
 
-uint16_t adc_read(void) {
+uint16_t temp_adc_read(void) {
   ADCSRA |= (1<<ADSC);
   do {} while (ADCSRA & (1<<ADSC));
   uint8_t temp_vall = ADCL;
@@ -12,13 +12,23 @@ uint16_t adc_read(void) {
   return temp_val;
 }
 
-void adc_init(int pin) {
+void adc_init(void) {
   ADMUX = (1<<REFS0)|(0<<ADLAR);
   DIDR0 |= (1<<ADC0D);
   ADCSRA = (1<<ADEN) | (1<<ADPS0) | (1<<ADPS1) ;
 
   DDRD = 0x00;
   DDRC = 0x00;
-  PORTC |= (1<<pin);
+  PORTC |= (1<<0)|(1<<1);
   ACSR = (1<<ACBG);
+}
+
+uint16_t light_adc_read(void) {
+  ADMUX = (1<<MUX0);
+  ADCSRA |= (1<<ADSC);
+  do {} while (ADCSRA & (1<<ADSC));
+  uint8_t temp_vall = ADCL;
+  uint8_t temp_valh = ADCH;
+  uint16_t temp_val = (temp_valh<<8) | (temp_vall);
+  return temp_val;
 }
