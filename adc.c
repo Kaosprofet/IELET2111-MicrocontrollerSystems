dc5133f840126656d51a2d794d307a43af38a0d2
Simen <simen@pop-os.localdomain>
2021-05-07 16:32:02 +0200

    Updated ADC

--- adc.c
+++ adc.c
@@ -3,18 +3,18 @@
 #include <avr/io.h>
 
 int adc_read(void) {
-   DDRD = 0x00;
-   DDRC = 0x00;
-   PORTC |= (1<<SENSOR);
-   ACSR = (1<<ACBG);
-   
-   do {
-     ADCSRA |= (1<<ADSC);
-   } while(1);
+  ADCSRA |= (1<<ADSC);
+  do {} while (ADCSRA & (1<<ADSC));
+  return ADC;
 }
 
 void adc_init(void) {
   ADMUX = (1<<REFS0);
   DIDR0 |= (1<<ADC0D);
   ADCSRA = (1<<ADEN);
+
+  DDRD = 0x00;
+  DDRC = 0x00;
+  PORTC |= (1<<SENSOR);
+  ACSR = (1<<ACBG);
 }
