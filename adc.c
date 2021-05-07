96620cdf7e7b341d4870365ea94d1d8227dd1da3
Simen Dager Sneve <smndagersneve@gmail.com>
2021-05-07 20:43:02 +0200

    Fixed my mistake

--- adc.c
+++ adc.c
@@ -1,15 +1,15 @@
-
 #include <avr/io.h>
 #include "adc.h"
 
 
 uint16_t temp_adc_read(void) {
+  ADMUX = (0<<MUX0);
   ADCSRA |= (1<<ADSC);
   do {} while (ADCSRA & (1<<ADSC));
-  uint8_t temp_vall = ADCL;
-  uint8_t temp_valh = ADCH;
-  uint16_t temp_val = (temp_valh<<8) | (temp_vall);
-  return temp_val;
+  uint8_t vall = ADCL;
+  uint8_t valh = ADCH;
+  uint16_t val = (valh<<8) | (vall);
+  return val;
 }
 
 void adc_init(void) {
@@ -27,8 +27,8 @@ uint16_t light_adc_read(void) {
   ADMUX = (1<<MUX0);
   ADCSRA |= (1<<ADSC);
   do {} while (ADCSRA & (1<<ADSC));
-  uint8_t temp_vall = ADCL;
-  uint8_t temp_valh = ADCH;
-  uint16_t temp_val = (temp_valh<<8) | (temp_vall);
-  return temp_val;
+  uint8_t vall = ADCL;
+  uint8_t valh = ADCH;
+  uint16_t val = (valh<<8) | (vall);
+  return val;
 }
